67c6364813d0bfa0ceb5bef84cd74b7a05b50aed
Mahmoud Adel <mahmoudadela413@gmal.com>
2024-02-08 21:33:16 -0800

    create splash view body

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/splash/pesentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,9 +11,8 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-        // home: SplashView(),
-
-        );
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
   }
 }
