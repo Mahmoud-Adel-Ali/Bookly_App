19d9d24734c8847fd942d42dfa7ebf972b59fe6e
Mahmoud Adel <mahmoudadela413@gmal.com>
2024-02-09 20:00:54 -0800

    create Custom AppBar in home

---
+++ lib/features/home/presentation/views/widgets/homeViewBody.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Column(
+        children: [
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
