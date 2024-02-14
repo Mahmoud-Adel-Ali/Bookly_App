2b0330bc7e0e5eb8c965e2e48190f554025d1a9f
Mahmoud Adel <mahmoudadela413@gmal.com>
2024-02-13 17:57:08 -0800

    styles part 2

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,9 +1,27 @@
+import 'package:bookly_app/constant.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  
   static const textSyle18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
+  );
+  static const textSyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: KGtSectraFine,
+  );
+  static const textSyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    fontFamily: KGtSectraFine,
+  );
+  static const textSyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textSyle16 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
   );
 }
